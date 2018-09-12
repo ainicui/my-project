0dac0a23314832a15a17abfe7042f6e732f87d42
along <qyl19930327@163.com>
2018-09-13 01:15:37 +0800

    “”

---
+++ src/components/aside.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="aside">
+    <el-checkbox-group v-model="isCollapse" width="100%">
+      <el-checkbox-button>{{tab}}</el-checkbox-button>
+    </el-checkbox-group>
+    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">导航一</span>
+        </template>
+        <el-menu-item-group>
+          <span slot="title">分组一</span>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <span slot="title">选项4</span>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 240px;
+  min-height: 400px;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      isCollapse: true
+    };
+  },
+  computed: {
+    tab() {
+      return this.isCollapse ? "展开" : "关闭";
+    }
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+};
+</script>
+<style lang="scss">
+.aside {
+  .el-checkbox-button {
+    width: 100%;
+  }
+}
+.el-checkbox-button:last-child .el-checkbox-button__inner {
+  border-radius: 0 !important;
+  border: none;
+  width: 100%;
+  height: 60px;
+  line-height: 60px;
+  padding: 0;
+}
+</style>
