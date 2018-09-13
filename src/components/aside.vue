af505fa81951c185231c15ea12fe83b77b8768b2
qinyla <qinyla@yutong.com>
2018-09-13 18:13:25 +0800

    cdshi

--- src/components/aside.vue
+++ src/components/aside.vue
@@ -1,36 +1,40 @@
 <template>
   <div class="aside">
-    <el-checkbox-group v-model="isCollapse" width="100%">
-      <el-checkbox-button>{{tab}}</el-checkbox-button>
-    </el-checkbox-group>
-    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span slot="title">导航一</span>
-        </template>
-        <el-menu-item-group>
-          <span slot="title">分组一</span>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <span slot="title">选项4</span>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
-      <el-menu-item index="3">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
-      </el-menu-item>
-    </el-menu>
+    <div class="logo">
+    <p>{{ tab }}</p>
+    </div>
+    <div class="aside-info">
+      <div class="info">
+        <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="true">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span slot="title">导航一</span>
+            </template>
+            <el-menu-item-group>
+              <span slot="title">分组一</span>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <span slot="title">选项4</span>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -44,6 +48,9 @@
 
 <script>
 export default {
+  props: {
+    isShow: Boolean
+  },
   data() {
     return {
       isCollapse: true
@@ -51,7 +58,7 @@ export default {
   },
   computed: {
     tab() {
-      return this.isCollapse ? "展开" : "关闭";
+      return 'long'
     }
   },
   methods: {
@@ -65,17 +72,35 @@ export default {
 };
 </script>
 <style lang="scss">
+.aside-active{
+    width: 241px;
+  }
 .aside {
-  .el-checkbox-button {
+  min-width: 65px;
+  height: 100%;
+  position: relative;
+  .aside-info{
     width: 100%;
+    top: 0;
+    left: 0;
+    position: absolute;
+    height: 100%;
+    padding-top: 60px;
+    box-sizing: border-box;
+    .info{
+      height: 100%;
+      border-right: solid 1px #e6e6e6;
+    }
+  }
+  .el-menu{
+    border: none;
   }
 }
-.el-checkbox-button:last-child .el-checkbox-button__inner {
-  border-radius: 0 !important;
-  border: none;
+.logo{
   width: 100%;
   height: 60px;
   line-height: 60px;
-  padding: 0;
+  color: #ffffff;
+  background: #508fe7
 }
 </style>
---
+++ src/views/talk_list/index.vue
@@ -0,0 +1,202 @@
+<template>
+  <div class="home">
+    <el-button type="primary" @click.native="showFrom" style="float: right; margin-bottom:10px;padding:10px 15px;">新增</el-button>
+    <el-table ref="multipleTable" :data="tableData" border tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55"></el-table-column>
+      <el-table-column label="日期" width="200">
+        <template slot-scope="scope">
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{ new Date(scope.row.date_time ).toLocaleString() }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="user_name" label="昵称" width="150"></el-table-column>
+      <el-table-column prop="content" label="内容"></el-table-column>
+      <el-table-column label="图片" width="300">
+        <template slot-scope="scope">
+          <img v-if="scope.row.imgUrl.length>0" :src="scope.row.imgUrl[0].url" alt="" srcset="" width="150" height="100" style="margin:auto">
+          <span v-else>无</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" width="180">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination :page-size="pageSize" :pager-count="11" layout="prev, pager, next" :total="num" @current-change="currentChange"
+    @prev-click="currentChange" @next-click="currentChange"></el-pagination>
+    <!-- 表单部分 -->
+    <el-dialog :title="title" :visible.sync="isVisible">
+      <div class="from-data">
+        <el-form label-width="80px">
+          <el-form-item label="昵称">
+            <el-input v-model="name" placeholder="请输入用户名" clearable class="_input"></el-input>
+          </el-form-item>
+          <el-form-item label="内容">
+            <el-input v-model="content" type="textarea" placeholder="请输入内容" clearable class="_input"></el-input>
+          </el-form-item>
+          <el-form-item label="图片">
+            <el-upload class="upload-demo" action="http://localhost:3000/upload" style="text-align:left" :on-success="imageSuccessed" :file-list="fileList" list-type="picture">
+              <el-button size="medium" type="primary">点击上传</el-button>
+            </el-upload>
+          </el-form-item>
+          <el-form-item label="">
+            <el-button @click.native="submit" type="primary" class="_button" style="width:100%">提交</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import $ajax from "axios";
+export default {
+  name: "home",
+  components: {},
+  data() {
+    return {
+      id: this.$route.query.id,
+      tableData: [],
+      multipleSelection: [],
+      isVisible: false,
+      name: "",
+      content: "",
+      fileList: [],
+      title: "新增列表",
+      pageSize: 5,
+      pageIndex: 1,
+      num: null
+    };
+  },
+  watch: {
+    isVisible(val) {
+      console.log(val);
+    }
+  },
+  methods: {
+    toggleSelection(rows) {
+      if (rows) {
+        rows.forEach(row => {
+          this.$refs.multipleTable.toggleRowSelection(row);
+        });
+      } else {
+        this.$refs.multipleTable.clearSelection();
+      }
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    handleDelete(index, item) {
+      this.$confirm("此操作将永久删除该数据, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          $ajax
+            .post("http://localhost:3000/del", {
+              id: item._id
+            })
+            .then(res => {
+              console.log(res);
+              if (res.data.code === 1) {
+                this.$message({
+                  type: "success",
+                  message: "删除成功!"
+                });
+                this.tableData.splice(index, 1);
+              }
+            })
+            .catch(err => {});
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+    showFrom() {
+      this.title = "新增列表";
+      this.id = "";
+      this.name = "";
+      this.content = "";
+      this.fileList = [];
+      this.isVisible = true;
+    },
+    handleEdit(index, val) {
+      this.isVisible = true;
+      this.title = "编辑列表";
+      this.id = val._id;
+      this.name = val.user_name;
+      this.content = val.content;
+      this.fileList = val.imgUrl;
+      console.log(val);
+    },
+    submit() {
+      let _this = this;
+      $ajax
+        .post("http://localhost:3000/users/from", {
+          user_name: this.name,
+          content: this.content,
+          imgUrl: this.fileList,
+          id: this.id
+        })
+        .then(res => {
+          console.log(res);
+          if (res.data.code == 1) {
+            this.$message({
+              type: "success",
+              message: res.data.msg,
+              onClose: function() {
+                _this.dialogFormVisible = false;
+                _this.$router.push({ path: "/" });
+              }
+            });
+          } else {
+            this.$message({
+              type: "info",
+              message: res.data.msg
+            });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    imageSuccessed: function(response, file, list) {
+      if (file.response.code === 1) {
+        this.fileList.push({
+          type: file.response.data.type,
+          url: file.response.data.url
+        });
+      } else {
+        alert("上传失败");
+      }
+      console.log(this.fileList);
+    },
+    dataInfo(pageSize, pageIndex) {
+      $ajax
+        .post("http://localhost:3000/all", {
+          pageSize: pageSize,
+          currentPage: pageIndex
+        })
+        .then(res => {
+          console.log(res);
+          this.tableData = res.data.data;
+          this.num = res.data.num;
+        })
+        .catch(err => {});
+    },
+    currentChange(val) {
+      this.dataInfo(this.pageSize,val)
+      console.log(val);
+    }
+  },
+  mounted() {
+    this.dataInfo(this.pageSize,this.pageIndex)
+  }
+};
+</script>
